d154d97eabcd3f1b9e41cab6205bbd4cce963ec2
wjcwqc <wjcwqc@gmail,com>
2019-10-13 00:37:59 +0800

    upload codes at 19.10.12
    Signed-off-by: wjcwqc <wjcwqc@gmail,com>

--- high-accuracy/Problem_1547.java
+++ high-accuracy/Problem_1547.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class Problem_1547 {
     public static void main(String[] args) {
-        Scanner cin=new Scanner(System.in);
-        while(cin.hasNext()){
-            BigInteger a=cin.nextBigInteger();
-            BigInteger b=cin.nextBigInteger();
+        Scanner cin = new Scanner(System.in);
+        while (cin.hasNext()) {
+            BigInteger a = cin.nextBigInteger();
+            BigInteger b = cin.nextBigInteger();
             System.out.println(a.add(b));
         }
     }
